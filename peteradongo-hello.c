5c78405a824185a5b76a9091dfce6dcaec822d04
ochiengpeter362-spec <ochiengpeter362@gmail.com>
2026-09-19 12:30:24 +0300

    Add files via upload

---
+++ peteradongo-hello.c
@@ -0,0 +1,24 @@
+//program to display hello world
+
+/*
+(mustiline comment)
+Author: Peter Adongo
+Reg number: BCS-01-0558/2026
+Descriptrion: Hello world program
+Date: 09/09/2026
+Version: 1
+*/
+
+//pre-processor directive
+#include <stdio.h> //scanf(), printf()
+#define PI 3.142
+
+int main() {
+    //display hello world
+    printf("hello world\n");
+    printf("Welcom to Zetech this is the home of future tech leaders\n");
+    printf("We learn programming languages such as\n");
+    printf("here are the languages below\n");
+    printf("Java, c++, python and javascript\n");
+    return 0;
+}
