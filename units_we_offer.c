a5a704232a7f34d3b73e7b95e3e92a0bfd493b5c
ochiengpeter362-spec <ochiengpeter362@gmail.com>
2026-09-19 12:27:19 +0300

    Add files via upload

---
+++ units_we_offer.c
@@ -0,0 +1,27 @@
+//program to diplay units offer this semester
+
+/*
+(multiline comment)
+Author: Peter Adongo
+Reg no: BCS-01-0558/2026
+Description Units offered this semester at Zetech university
+Date: 16/09/2026
+Version: 1
+*/
+
+//pre-processor directive
+#include <stdio.h> //scanf(), printf()
+int main(){
+	//display units offered at zetech university
+	printf("Welcome to zetech university \n");
+	printf("We offer the following units this year \n");
+	printf("1. Structured programing and Algorithms \n");
+	printf("2. Computational Logic \n");
+	printf("3. Communication Skills \n");
+	printf("4. Basic Mathematics \n");
+	printf("5. Health and Wellnes management \n");
+	printf("6. Introduction to physics \n");
+	printf("7. Introduction to computer and Internets \n");
+	
+	return 0;
+}
